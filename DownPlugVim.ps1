49d4a7cde26b05659f20436984ac324d8785e755
bickman <bickman@qq.com>
2020-01-01 22:19:05 +0800

    increase download speed

--- DownPlugVim.ps1
+++ DownPlugVim.ps1
@@ -5,4 +5,9 @@
 #"RemoteSigned": you can run remote script which is signed and local script,
 #"Unrestricted": you can run any script
 # use "Set-ExecutionPolicy RemoteSigned" in PowerShell with Administration to change it.   
-Invoke-WebRequest  "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile ".\autoload\plug.vim"
+#Method 1:
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest  "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -UseBasicParsing -OutFile ".\autoload\plug.vim"
+
+#Method 2:
+#(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",".\autoload\plug.vim")
