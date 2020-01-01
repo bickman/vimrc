f8c400109ce23e0f1bc91f4aecffa87a07c97345
bickman <bickman@qq.com>
2020-01-01 21:29:11 +0800

    Update ps1 script and readme.md

--- DownPlugVim.ps1
+++ DownPlugVim.ps1
@@ -1,1 +1,8 @@
+#this script download the newest plug.vim file in autoload directory for you.
+#note: if you can't run script in powershell ,please use "Get-ExecutionPolicy" in PowerShell 
+#to check the state witch would result in "Restrict": you can't run any script,
+#"AllSigned": you can only run script which is signed,
+#"RemoteSigned": you can run remote script which is signed and local script,
+#"Unrestricted": you can run any script
+# use "Set-ExecutionPolicy RemoteSigned" in PowerShell with Administration to change it.   
 Invoke-WebRequest  "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -OutFile ".\autoload\plug.vim"
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # My vimrc file
 My vimrc use [vim-plug](https://github.com/junegunn/vim-plug) as plugin manager.  
 Use `git clone https://github.com/bickman/vimrc` to download vimrc.   
-Use the **vim-plug.html** or [here](https://github.com/junegunn/vim-plug) to download `vim-plug` and put it in `autoload`.
+Run the PowerShell Script "DownPlugVim.ps1" or [here](https://github.com/junegunn/vim-plug) to download "Plug.vim" file and put it in "autoload" directory.
